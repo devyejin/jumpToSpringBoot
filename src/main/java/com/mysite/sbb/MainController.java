7fbd0eb094e535f748b48889f16c598b222ebf4b
yejinoh58 <yejinoh58@gmail.com>
2023-06-13 18:05:15 +0900

    incomplete

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -1,11 +1,16 @@
 package com.mysite.sbb;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.security.Principal;
+@Slf4j
 @Controller
 public class MainController {
+
 
     @GetMapping("/sbb")
     @ResponseBody
@@ -14,7 +19,14 @@ public class MainController {
     }
 
     @GetMapping("/")
-    public String home() {
+    public String home(Principal principal, Model model) {
+        
+        if(principal != null) {
+            String loginUserName = principal.getName();
+            log.info("loginUserName={}", loginUserName);
+            model.addAttribute("loginUserName", loginUserName);
+        }
+        
         return "redirect:/question/list";
     }
 }
