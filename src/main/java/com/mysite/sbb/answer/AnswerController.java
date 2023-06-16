11f0c08259fbc2f49322ee94097e5737393443b0
yejinoh58 <yejinoh58@gmail.com>
2023-06-16 20:57:26 +0900

    feature answer modify

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -1,22 +1,26 @@
 package com.mysite.sbb.answer;
 
+import com.mysite.sbb.DataNotFoundException;
 import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionForm;
 import com.mysite.sbb.question.QuestionService;
 import com.mysite.sbb.user.SiteUser;
 import com.mysite.sbb.user.UserService;
+import com.sun.tools.jconsole.JConsoleContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.security.Principal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Slf4j
 @Controller
@@ -24,41 +28,89 @@ import java.security.Principal;
 @RequestMapping("/answer")
 public class AnswerController {
 
-        private final QuestionService questionService; //answer등록하는데 왜 question?
-        private final AnswerService answerService;
+    private final QuestionService questionService; //answer등록하는데 왜 question?
+    private final AnswerService answerService;
 
-        private final UserService userService;
+    private final UserService userService;
 
-        @PreAuthorize("isAuthenticated()")
-        @PostMapping("/create/{id}")
-        public String createAnswer(
-                Model model,
-                @PathVariable("id") Integer id,
-                @Valid AnswerForm answerForm, BindingResult bindingResult,
-                        Principal principal
-                )
-        {
-                //작성자 정보
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/create/{id}")
+    public String createAnswer(
+            Model model,
+            @PathVariable("id") Integer id,
+            @Valid AnswerForm answerForm, BindingResult bindingResult,
+            Principal principal
+    ) {
+        //작성자 정보
 //                String username = principal.getName();
 //                SiteUser author = this.userService.getUser(username);
-                SiteUser siteUser = this.userService.getUser(principal.getName());
+        SiteUser siteUser = this.userService.getUser(principal.getName());
 
 
-                //해당 질문을 가져와야해서 questionService가 필요
-                Question question = this.questionService.getQuestion(id);
+        //해당 질문을 가져와야해서 questionService가 필요
+        Question question = this.questionService.getQuestion(id);
 
-                //대안 로직
-                if(bindingResult.hasErrors()) {
-                        model.addAttribute("question", question); //몇번째 질문인지 정보 담아줘야함.
-                        return "question_detail";
-                }
-
-                //정상로직
-
-                this.answerService.create(question,answerForm.getContent(),siteUser);
-                //답변 저장하고나면 보낼 페이지, 질문 페이지
-                return String.format("redirect:/question/detail/%s",id); //문자열 형식 지정해주는 메서드
-
+        //대안 로직
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question); //몇번째 질문인지 정보 담아줘야함.
+            return "question_detail";
         }
+
+        //정상로직
+
+        this.answerService.create(question, answerForm.getContent(), siteUser);
+        //답변 저장하고나면 보낼 페이지, 질문 페이지
+        return String.format("redirect:/question/detail/%s", id); //문자열 형식 지정해주는 메서드
+
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String modify(AnswerForm answerForm, //요청으로부터 DTO받아올때는 선두에 놔야함
+                         @PathVariable("id") Integer id,
+                         Principal principal
+                         ) {
+
+        log.info("message={}","GET 호출");
+        Answer answer = this.answerService.getAnswer(id); //답변 찾음, 내용을 뷰로 보내줘야함
+
+        //더블 체킹 (답변 작성자랑 현재 사용자(Principal)
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+        }
+        //정보 보내주기
+        answerForm.setContent(answer.getContent()); //DTO에 담은건 model에 자동으로 add됨 , 이때는 수정날짜는저장이 안될텐데
+        // 기존 답변 answerForm 받아온거에서 Content값만 수정한거같음!
+
+        return "answer_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String modify(@Valid AnswerForm answerForm,
+                         BindingResult bindingResult,
+                         @PathVariable("id") Integer id,
+                         Principal principal) {
+
+        log.info("message={}","post 호출");
+        if(bindingResult.hasErrors()) {
+            log.error("error={}",bindingResult.hasErrors());
+//            return String.format("redirect:/answer/modify/%c",id);
+            return "answer_form";
+        }
+
+        //정상 로직
+        Answer answer = this.answerService.getAnswer(id);
+
+        //더블 체킹
+        if(!answer.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+        }
+        log.info("content={}", answerForm.getContent());
+        // 매개변수로 answer PK 를 넘기는게 더 깔끔하지 않나?? 더블체킹한 값으로 넘기는게 더 좋은건가?
+        this.answerService.modify(answer, answerForm.getContent()); //기존 답변, 새로작성한 답변내용
+
+        return String.format("redirect:/question/detail/%s",answer.getQuestion().getId()); //질문페이지 출력이니까
+    }
 
 }
--- src/main/java/com/mysite/sbb/answer/AnswerService.java
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -1,6 +1,7 @@
 package com.mysite.sbb.answer;
 
 
+import com.mysite.sbb.DataNotFoundException;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.user.SiteUser;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 
 @Slf4j
@@ -23,6 +25,31 @@ public class AnswerService {
         answer.setCreateDate(LocalDateTime.now());
         answer.setQuestion(question);
         answer.setAuthor(author);
+        this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+
+        if(answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found.");
+        }
+
+    }
+
+    public void modify(Answer answer, String content) { //content보다 modifyContent가 명확해보이는데..
+//        Optional<Answer> findAnswer = this.answerRepository.findById(answer.getId());
+//
+//        if(!findAnswer.isPresent()) {
+//            throw new DataNotFoundException("answer is not found.");
+//        }
+//
+//        Answer answer1 = findAnswer.get();
+        //-> 이미 answer이 repository에서 찾은 값이니까 똑같은 작업을 반복할 이유가 없음! 바로 변경 데이터 담아주기
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
         this.answerRepository.save(answer);
     }
 }
